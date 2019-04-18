ffe4b54cc8566af8eb1e94bab2b8f66d01d15242
Gian Lorenzo <gian.comandini@gmail.com>
2019-04-18 12:36:35 +0200

    Added <Header> to the skeleton

--- frontend/app/src/components/App/App.js
+++ frontend/app/src/components/App/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import Canvas from '../Canvas/Canvas';
 import Chat from '../Chat/Chat';
+import Header from '../Header/Header';
+
+import './App.scss'
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <Canvas />
         <Chat />
       </div>
---
+++ frontend/app/src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+class Header extends Component {
+  render() {
+    return (
+        <div className="Header">
+            <AppBar position="static" color="default">
+                <Toolbar>
+                    <Typography variant="h6" color="inherit">
+                        Soctionary
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+  }
+}
+
+export default Header;
