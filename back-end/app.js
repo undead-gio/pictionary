26c58eed4a62698b3f33c6372a98c64bb5018e63
undead-gio <gio.com@hotmail.it>
2019-05-09 10:29:13 +0200

    change structure of backend

--- back-end/app.js
+++ back-end/app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var cors = require('cors');
 var http = require('http').Server(app);
-var io = require('socket.io')(server);
+var io = require('socket.io')(http);
 var app = express();
 
 app.use(cors());
@@ -36,6 +36,12 @@ var NAME = [
   "ciccio", "boro", "er ciavatta", "er ciofeca", "mi'zia", "secco", "tu'madre",
   "caciarone", "er cipolla"
 ]
+
+// test html
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
 // event start for new incoming connection
 io.on('connection', function (socket){
   console.log('New user connected');
@@ -53,81 +59,38 @@ io.on('connection', function (socket){
        usersArray = [io.sockets.clients()];
        //connectedUsersArray = Object.getOwnPropertyNames(usersArray[0].server.nsps['/'].sockets);
        //When a user disconnects I will be emitting the new total user
-       socket.broadcast.emit('/', totalUsers);
+       socket.broadcast.emit('/', { totUser: totalUsers });
+       console.log('New user connected');
    });
 
   // event start when recive a message from frontEnd
-  socket.on('/', function (data) {
-    // switch case to controll the type of message, and do for specific type specific operation
-    switch (data.msgType) {
+  socket.on('start', function (data) {
+    var master = connectedUsersArray[Math.floor(Math.Random() * totalUsers)];
+    io.sockets.emit('start', {  master: master});
+  });
 
-      case "draw":
-        line_history.push(data.line); // add received line to history
-        io.sockets.emit('/', { line: data.line });  // send line to all clients
-        break;
+  socket.on('draw', function (data) {
+    line_history.push(data.line); // add received line to history
+    io.sockets.emit('draw', { line: data.line });  // send line to all clients
+  });
 
-      case "chat":
-        if( data.message == WORDS[randomNumb] ){
-          io.sockets.emit('/', { win: true, winner: socket.username, winWord: WORDS[randomNumb] })
-        }
-        else {
-          io.sockets.emit('/', { message: data.message, username: socket.username })
-        }
-        break;
-
-      case "master":
-        var master = connectedUsersArray[Math.floor(Math.Random() * totalUsers)]
-        io.sockets.emit('/', { master: master})
-        break;
-
-      case "word":
-        var numWords = WORDS.lenght();  // lennght of array words
-        randomNumb = Math.floor(Math.random() * numWords);
-        io.broadcast.emit('/', { word: WORDS[randomNumb] });
-        break;
-
-      default:
-        console.log("not correct data")
+  socket.on('chat', function (data) {
+    /*if( data.message == WORDS[randomNumb] ){
+      io.sockets.emit('/', { type: "outcome", win: true, winner: socket.username, winWord: WORDS[randomNumb] })
     }
+    else {
+      io.sockets.emit('/', { type: "chat", message: data.message, username: socket.username })
+    }*/
+    io.sockets.emit('chat', { message: data.message })
+  });
+
+  socket.on('word', function (data) {
+      var numWords = WORDS.lenght();  // lennght of array words
+      randomNumb = Math.floor(Math.random() * numWords);
+      io.broadcast.emit('word', { word: WORDS[randomNumb] });
   });
 
 });
-
-/*var expressWs = require('express-ws')(app);
-var aWss = expressWs.getWss('/');
-app.ws('/', function(req, res) {
-  switch (res) {
-    case "chat":
-      chat(res.message);
-      break
-
-    case "draw":
-      //inviare a tutti tranne che al master cordinate x e y per il canvas
-      break;
-
-    case "master":
-      //controllo della listra di client e assegnare ad uno random il master
-      break
-
-    case "word":
-      //inviare solo al master una parole scelta random da un dizionario predefinito
-      word(res)
-      break
-  }
-});
-
-function chat = (res) => {
-  if(res == rdmWord){
-    "vincitore";
-  }
-  else{
-    ws.on('message', function(msg) {
-      aWss.clients.forEach(function (client) {
-        client.send(msg);
-      });
-    });
-  };
-}*/
 
 
 app.listen(3000);
