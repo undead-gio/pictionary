2ecde5c027035630448ea236c7aed7096f0837f3
Gian Lorenzo <gian.comandini@gmail.com>
2019-05-09 13:00:26 +0200

    initialized chat bar

--- frontend/app/src/components/Chat/Chat.js
+++ frontend/app/src/components/Chat/Chat.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Fab from '@material-ui/core/Fab';
+import IconButton from '@material-ui/core/IconButton';
+import SendIcon from '@material-ui/icons/Send';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
 
 class Chat extends Component {
   render() {
     return (
+      //da spostare la barra nel footer
       <div className="Chat">
-        <h1>Chat</h1>
+      
+        <AppBar position="fixed" color="primary" >
+          <Toolbar >
+            <TextField
+            id="outlined-full-width"
+            label="Guess the draw"
+            style={{ margin: 8 }}
+            placeholder="Placeholder"
+            margin="normal"
+            variant="outlined"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            />
+            <div>
+              <Fab size="medium" color="secondary" aria-label="Add">
+                <SendIcon />
+              </Fab>
+            </div>
+          </Toolbar>
+        </AppBar>
+        
+
       </div>
     );
   }
