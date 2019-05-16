3f55c4e02c69932859879ad53fdde6c51f2b1bc8
undead-gio <gio.com@hotmail.it>
2019-05-16 10:42:40 +0200

    add username and list of connected user

--- back-end/app.js
+++ back-end/app.js
@@ -8,6 +8,12 @@ var server =  http.createServer(app);
 var io = socketIo.listen(server);
 server.listen(8080);
 
+// array of names for users
+var NAMES = [
+  "ciccio", "boro", "er ciavatta", "er ciofeca", "mi'zia", "secco", "tu'madre",
+  "caciarone", "er cipolla"
+]
+
 // add directory with our static files
 app.use(express.static(__dirname + '/public'));
 console.log("Server running on 127.0.0.1:8080");
@@ -18,23 +24,39 @@ var line_history = [];
 // event-handler for new incoming connections
 io.on('connection', function (socket) {
 
-   // first send the history to the new client
-   for (var i in line_history) {
-      socket.emit('draw', { line: line_history[i] } );
-   }
+  // list of connected user
+  let connectedUsersArray = Object.keys(io.sockets.sockets);
+  // assign random username to new connected user
+  socket.username = NAME[Math.floor(Math.random() * NAMES.length];
+  // emit with socket the list of connected user
+  socket.broadcast.emit('connect', { totUser: connectedUsersArray.length });
 
-   // add handler for message type "draw_line".
-   socket.on('draw', function (data) {
-      // add received line to history
-      line_history.push(data.line);
-      // send line to all clients
-      io.emit('draw', { line: data.line });
+  // first send the history to the new client
+  for (var i in line_history) {
+    socket.emit('draw', { line: line_history[i] } );
+  }
+
+  // event start when someone disconnect
+  socket.on('disconnect', function() {
+       // when a user disconnects I will be emitting the new total user
+       let connectedUsersArray = Object.keys(io.sockets.sockets);
+       // emit  with broadcast the new list of users connected
+       socket.broadcast.emit('disconnect', { totUser: connectedUsersArray.length });
+       console.log('disconnected' + socket.username);
    });
 
-   // add handler for message type "chat".
-   socket.on('chat message', function(msg){
-    //send message to all clients
-    io.emit('chat message', msg);
+  // add handler for message type "draw_line".
+  socket.on('draw', function (data) {
+    // add received line to history
+    line_history.push(data.line);
+    // send line to all clients
+    io.emit('draw', { line: data.line });
+  });
+
+  // add handler for message type "chat".
+  socket.on('chat message', function(msg){
+  // send message to all clients
+  io.emit('chat message', { msg: msg, username: socket.username });
   });
 
 });
