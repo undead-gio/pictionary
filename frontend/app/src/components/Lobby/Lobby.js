3c2c3391168aa952709dd1919b5b4ccb3541a254
Matteo Foroni <matteo.foroni97@gmail.com>
2019-05-16 12:24:26 +0200

    Created Lobby folder

---
+++ frontend/app/src/components/Lobby/Lobby.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import server from 'socket.io-client';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import FolderIcon from '@material-ui/icons/Folder';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+class Lobby extends Component {
+    render() {
+        return (
+            <Grid item xs={12} md={6}>
+                <Typography variant="h6" className={classes.title}>
+                    Connected users
+                </Typography>
+                <div className={classes.demo}>
+                    <List dense={dense}>
+                        {generate(
+                            <ListItem>
+                                <ListItemAvatar>
+                                    <Avatar>
+                                        <FolderIcon />
+                                    </Avatar>
+                                </ListItemAvatar>
+                                <ListItemText
+                                    primary="nome utente"
+                                    secondary={secondary ? 'Secondary text' : null}
+                                />
+                            </ListItem>,
+                        )}
+                    </List>
+                </div>
+            </Grid>
+        );
+    }
+}
+        
